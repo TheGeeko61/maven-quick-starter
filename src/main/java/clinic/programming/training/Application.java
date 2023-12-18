1eb21546b9cc4b6c7a3b3413bcb1ebd2440eabbc
Kameahāʻaweokaponia M'Intosh-ʻiʻi <thegeeko61@gmail.com>
2023-12-17 22:18:34 -0600

    Testing, testing...

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,19 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
-    
+   
+    public void greet() {
+    	List<String> greetings = new ArrayList<>();
+	greetings.add("Hello");
+
+	for (String greeting:greetings) {
+		System.out.println("Greeting:  " + greeting);
+	}
+    }
+
     public Application() {
         System.out.println ("Inside Application");
     }
