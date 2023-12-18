7e89d58d797f9e2619761fed4b95ced40bd44fae
Kameahāʻaweokaponia M'Intosh-ʻiʻi <thegeeko61@gmail.com>
2023-12-17 22:29:01 -0600

    Message whatever

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
 public class Application {
-   
+  
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, ' ');
+	return (separateWords == null)? 0 : separateWords.length;
+    }
+
     public void greet() {
     	List<String> greetings = new ArrayList<>();
 	greetings.add("Hello");
@@ -23,5 +28,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
+        int count = app.countWords("I have four words");
+	System.out.println("Word count:  " + count);
     }
 }
